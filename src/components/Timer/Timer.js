19066e82bd54294e3f04f5ff6fb1261367f63561
Joshua Halls <joshuah40@googlemail.com>
2020-03-08 18:37:49 +0000

    started first component

---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Timer extends React.Component {
+
+  render() {
+    return (
+      <div>
+          <h1> Test </h1>
+      </div>
+    )
+  }
+}
+
+export default Timer;
