51af38a22daa0a87e87b400396f436732c485d2f
Joshua Halls <joshuah40@googlemail.com>
2020-03-18 18:41:26 +0000

    Updates

---
+++ precache-manifest.de420f302c84c349e91a8ac04f66b1b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71208a5677357e2ce470711d57c656aa",
+    "url": "/clicker/index.html"
+  },
+  {
+    "revision": "f4725ccf5bd8dfa32675",
+    "url": "/clicker/static/css/main.4529386c.chunk.css"
+  },
+  {
+    "revision": "c7123016bc7aa0f206bc",
+    "url": "/clicker/static/js/2.2ee9ba52.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/clicker/static/js/2.2ee9ba52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4725ccf5bd8dfa32675",
+    "url": "/clicker/static/js/main.bc3fce59.chunk.js"
+  },
+  {
+    "revision": "3112cb266ec356c76ee2",
+    "url": "/clicker/static/js/runtime-main.2292aea3.js"
+  }
+]);
